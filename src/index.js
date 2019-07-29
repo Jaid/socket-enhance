a4c8238f5273bde029dc330353508897c83251f8
Jaid <jaid.jsx@gmail.com>
2019-07-29 21:30:58 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,88 @@
 /** @module socket-enhance */
 
+import socketioWildcard from "socketio-wildcard"
+import epochSeconds from "epoch-seconds"
+
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
- * @function
- * @returns {number} Seconds since epoch
- * @example
- * import socketEnhance from "socket-enhance"
- * const result = socketEnhance()
- * result === 1549410770
+ * @typedef {Object} JaidLoggerLike
+ * @prop {Function} error
+ * @prop {Function} warn
+ * @prop {Function} info
+ * @prop {Function} debug
  */
-export default () => Math.floor(Date.now() / 1000)
+
+/**
+ * @typedef {Object} ConstructorOptions
+ * @prop {JaidLoggerLike} logger
+ * @prop {string} [loggingLevel="info"]
+ */
+
+/**
+ * @typedef {Object} Options
+ * @prop {JaidLoggerLike} logger
+ */
+
+/**
+ * @class
+ * @example
+ * import socketIo from "socket.io"
+ * import {enhanceSocketServer} from "socket-enhance"
+ * const server = socketIo(1234)
+ * enchanceSocketServer(server)
+ */
+export default class SocketEnhancer {
+
+  /**
+   * @constructor
+   * @param {ConstructorOptions} options
+   */
+  constructor({logger, loggingLevel = "info"}) {
+    this.log = function () {
+      logger[loggingLevel](...arguments)
+    }
+  }
+
+  /**
+   * @return {import("socketio-wildcard").default}
+   */
+  getServerWildcardMiddleware() {
+    if (!this.serverWildcardMiddleware) {
+      this.serverWildcardMiddleware = socketioWildcard()
+    }
+    return this.serverWildcardMiddleware
+  }
+
+  /**
+   * @param {import("socket.io").Server} server
+   */
+  enhanceServer(server) {
+    server.use(this.getServerWildcardMiddleware())
+    server.on("connection", serverSocket => {
+      this.enhanceServerSocket(serverSocket)
+    })
+  }
+
+  /**
+   * @param {import("socket.io").Socket} serverSocket
+   */
+  enhanceServerSocket(serverSocket) {
+    const timestamp = epochSeconds()
+    this.log("◀︎ New socket %s from %s", serverSocket.id, serverSocket.handshake.address)
+    serverSocket.on("*", packet => {
+      const [eventName, ...payload] = packet.data
+      this.log("◀︎ %s [%s] %s", serverSocket.id, eventName, payload.join(" "))
+    })
+    const serverSocketEmit = serverSocket.emit
+    serverSocket.emit = (...args) => {
+      const [eventName, ...payload] = args
+      if (!["disconnecting", "disconnect"].includes(eventName)) {
+        this.log("▶︎ %s [%s] %s", serverSocket.id, eventName, payload.join(" "))
+      }
+      serverSocketEmit.apply(serverSocket, args)
+    }
+    serverSocket.on("disconnect", () => {
+      this.log("Socket %s disconnected after %s seconds", serverSocket.id, epochSeconds() - timestamp)
+    })
+  }
+
+}
--- test/test.js
+++ test/test.js
@@ -1,13 +1,39 @@
 import path from "path"
 
+import socketIo from "socket.io"
+import socketIoClient from "socket.io-client"
+import jaidLogger from "jaid-logger"
+import delay from "delay"
+import ms from "ms.macro"
+
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
  * @type { import("../src") }
  */
-const {default: socketEnhance} = indexModule
+const {default: SocketEnhancer} = indexModule
 
-it("should run", () => {
-  const result = socketEnhance()
-  expect(result).toBeGreaterThan(1549410770)
-})
+it("should run", async () => {
+  let communicationWorks = false
+  const logger = jaidLogger(`${_PKG_NAME}-test`)
+  const enhancer = new SocketEnhancer({logger})
+  const server = socketIo()
+  enhancer.enhanceServer(server)
+  server.on("connection", socket => {
+    server.client = socket
+    socket.on("hey", name => {
+      logger.info("I am %s!", name)
+      socket.emit("howdy", "🤠", "🐎")
+    })
+  })
+  server.listen(44911)
+  const client = socketIoClient("ws://localhost:44911")
+  client.on("howdy", () => {
+    communicationWorks = true
+  })
+  client.emit("hey", "you", Buffer.from("abc"))
+  await delay(ms`3 seconds`)
+  client.close()
+  server.close()
+  expect(communicationWorks).toBe(true)
+}, ms`10 seconds`)
